bbcc25701223ae5b52beebedf518b3915fba2b8d
Raphael Tarita <raphael.tarita@gmail.com>
2021-12-01 11:14:55 +0100

    [SOLUTION] Day 1

--- src/main/kotlin/io/github/raphaeltarita/days/Day1.kt
+++ src/main/kotlin/io/github/raphaeltarita/days/Day1.kt
@@ -2,16 +2,32 @@ package io.github.raphaeltarita.days
 
 import io.github.raphaeltarita.structure.AoCDay
 import io.github.raphaeltarita.util.day
+import io.github.raphaeltarita.util.pathOfDay
 import kotlinx.datetime.LocalDate
+import kotlin.io.path.readLines
 
 object Day1 : AoCDay {
     override val day: LocalDate = day(1)
 
+    private fun getInts(): Sequence<Int> {
+        return pathOfDay(1).readLines()
+            .asSequence() // not strictly necessary but might increase performance
+            .map { it.toInt() }
+    }
+
     override fun executePart1() {
-        TODO("Day 1 (part 1) not implemented yet")
+        val result = getInts().zipWithNext()
+            .count { (a, b) -> b > a }
+
+        println(result)
     }
 
     override fun executePart2() {
-        TODO("Day 1 (part 2) not implemented yet")
+        val result = getInts().windowed(3)
+            .map { it.sum() }
+            .zipWithNext()
+            .count { (a, b) -> b > a }
+
+        println(result)
     }
 }
--- src/main/kotlin/io/github/raphaeltarita/util/helpers.kt
+++ src/main/kotlin/io/github/raphaeltarita/util/helpers.kt
@@ -1,9 +1,11 @@
 package io.github.raphaeltarita.util
 
+import kotlinx.datetime.LocalDate
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.io.path.Path
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.pow
@@ -458,6 +460,14 @@ fun Path.renameTo(new: String): Path {
 
 fun existsOrNull(file: File): File? = if (file.exists()) file else null
 fun existsOrNull(file: Path): Path? = if (Files.exists(file)) file else null
+
+fun pathOfDay(day: LocalDate): Path = Path("input/day${day.dayOfMonth}.txt")
+fun pathOfDay(num: Int): Path = pathOfDay(day(num))
+fun pathToday(): Path = pathOfDay(today())
+
+fun fileOfDay(day: LocalDate): File = File("input/day${day.dayOfMonth}.txt")
+fun fileOfDay(num: Int): File = fileOfDay(day(num))
+fun fileToday(): File = fileOfDay(today())
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 // generic type operations
