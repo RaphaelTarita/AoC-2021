85fe8316001d844b3a3b89e7cb58ae2f04ffe544
Raphael Tarita <raphael.tarita@gmail.com>
2021-12-02 10:32:37 +0100

    [SOLUTION] Day 2

--- src/main/kotlin/io/github/raphaeltarita/days/Day2.kt
+++ src/main/kotlin/io/github/raphaeltarita/days/Day2.kt
@@ -2,16 +2,44 @@ package io.github.raphaeltarita.days
 
 import io.github.raphaeltarita.structure.AoCDay
 import io.github.raphaeltarita.util.day
+import io.github.raphaeltarita.util.inputPath
 import kotlinx.datetime.LocalDate
+import kotlin.io.path.readLines
 
 object Day2 : AoCDay {
     override val day: LocalDate = day(2)
 
-    override fun executePart1(): String {
-        return "Day 2 (part 1) not implemented yet"
+    private fun getPairs(): Sequence<Pair<String, Int>> {
+        return inputPath.readLines()
+            .asSequence()
+            .map { it.split(' ') }
+            .map { (dir, v) -> dir to v.toInt() }
     }
 
-    override fun executePart2(): String {
-        return "Day 2 (part 2) not implemented yet"
+    override fun executePart1(): Int {
+        val pair = getPairs().fold(0 to 0) { (horizontal, depth), (dir, v) ->
+            when (dir) {
+                "forward" -> (horizontal + v) to depth
+                "down" -> horizontal to (depth + v)
+                "up" -> horizontal to (depth - v)
+                else -> error("unknown direction: $dir")
+            }
+        }
+
+        return pair.first * pair.second
+    }
+
+    override fun executePart2(): Int {
+        val pair = getPairs()
+            .fold(Triple(0, 0, 0)) { (horizontal, depth, aim), (dir, v) ->
+                when (dir) {
+                    "forward" -> Triple(horizontal + v, depth + aim * v, aim)
+                    "down" -> Triple(horizontal, depth, aim + v)
+                    "up" -> Triple(horizontal, depth, aim - v)
+                    else -> error("unknown direction: $dir")
+                }
+            }
+
+        return pair.first * pair.second
     }
 }
