edc6352bc559a388befe854705531b5be31c53b0
Raphael Tarita <raphael.tarita@gmail.com>
2021-12-03 10:04:15 +0100

    [SOLUTION] Day 3

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
-![](https://img.shields.io/badge/day%20📅-2-yellow)
-![](https://img.shields.io/badge/stars%20⭐-4-blue)
-![](https://img.shields.io/badge/days%20completed-2-green)
+![](https://img.shields.io/badge/day%20📅-3-yellow)
+![](https://img.shields.io/badge/stars%20⭐-6-blue)
+![](https://img.shields.io/badge/days%20completed-3-green)
+
 # AoC-2021
+
 My solutions for the Advent of Code 2021 (in Kotlin)
 
 ### Contents:
+
 - input: All input files, named as `day${day-of-month}.txt`
-- output: Output files containing results (and possibly benchmarks) for the different days. Named either `day${day-of-month}.txt` for a single day or `days.txt`for all outputs in one file
+- output: Output files containing results (and possibly benchmarks) for the different days. Named
+  either `day${day-of-month}.txt` for a single day or `days.txt`for all outputs in one file
 - src/main/kotlin: Kotlin source root
     - io.github.raphaeltarita: packagae root
-        - days: package containing all AoC day solutions, beginning with a (fallback) Day 0 and going to Day 25. Named `Day${day-of-month}.kt`
-        - structure: package containing the infrastructure which defines how AoC Day challenges are defined, executed, benchmarked, outputted and printed to files
+        - days: package containing all AoC day solutions, beginning with a (fallback) Day 0 and going to Day 25.
+          Named `Day${day-of-month}.kt`
+        - structure: package containing the infrastructure which defines how AoC Day challenges are defined, executed,
+          benchmarked, outputted and printed to files
             - fmt: utility classes helping me to format the outputs of AoC challenges
         - util: package for `.kt` files which contain top-level / extension utilities
             - `internal.kt`: internal utilites that are useful for maintaining the infrastructure
--- src/main/kotlin/io/github/raphaeltarita/days/Day3.kt
+++ src/main/kotlin/io/github/raphaeltarita/days/Day3.kt
@@ -2,16 +2,62 @@ package io.github.raphaeltarita.days
 
 import io.github.raphaeltarita.structure.AoCDay
 import io.github.raphaeltarita.util.day
+import io.github.raphaeltarita.util.inputPath
 import kotlinx.datetime.LocalDate
+import kotlin.io.path.readLines
 
 object Day3 : AoCDay {
     override val day: LocalDate = day(3)
 
-    override fun executePart1(): String {
-        return "Day 3 (part 1) not implemented yet"
+    private fun getInputMatrix(): List<List<Char>> {
+        return inputPath.readLines()
+            .map { it.toList() }
     }
 
-    override fun executePart2(): String {
-        return "Day 3 (part 2) not implemented yet"
+    override fun executePart1(): Int {
+        val input = getInputMatrix()
+        val isize = input.size
+        val linesize = input.first().size
+        val gamma = StringBuilder()
+        val epsilon = StringBuilder()
+
+        for (i in 0 until linesize) {
+            val zeros = input.count { it[i] == '0' }
+            if (zeros * 2 > isize) {
+                gamma.append('0')
+                epsilon.append('1')
+            } else {
+                gamma.append('1')
+                epsilon.append('0')
+            }
+        }
+
+        return gamma.toString().toInt(2) * epsilon.toString().toInt(2)
+    }
+
+    private fun doFilter(pos: Int, input: List<List<Char>>, majority: Boolean): List<List<Char>> {
+        val (zeros, ones) = input.partition { it[pos] == '0' }
+        return if (zeros.size > ones.size == majority) {
+            zeros
+        } else {
+            ones
+        }
+    }
+
+    override fun executePart2(): Int {
+        val input = getInputMatrix()
+        var oxygen = input
+        var co2 = input
+        val linesize = input.first().size
+
+        for (i in 0 until linesize) {
+            if (oxygen.size > 1) {
+                oxygen = doFilter(i, oxygen, true)
+            }
+            if (co2.size > 1) {
+                co2 = doFilter(i, co2, false)
+            }
+        }
+        return oxygen.single().joinToString("").toInt(2) * co2.single().joinToString("").toInt(2)
     }
 }
