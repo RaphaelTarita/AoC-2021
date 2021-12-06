a83e8c56172f5afdf7f70dc5dd13ad2b53677b34
Raphael Tarita <raphael.tarita@gmail.com>
2021-12-06 10:08:06 +0100

    [SOLUTION] Day 6

--- src/main/kotlin/io/github/raphaeltarita/days/Day6.kt
+++ src/main/kotlin/io/github/raphaeltarita/days/Day6.kt
@@ -2,16 +2,42 @@ package io.github.raphaeltarita.days
 
 import io.github.raphaeltarita.structure.AoCDay
 import io.github.raphaeltarita.util.day
+import io.github.raphaeltarita.util.inputPath
 import kotlinx.datetime.LocalDate
+import kotlin.io.path.readText
 
 object Day6 : AoCDay {
     override val day: LocalDate = day(6)
 
-    override fun executePart1(): String {
-        return "Day 6 (part 1) not implemented yet"
+    private fun getInput(): List<Int> {
+        return inputPath.readText()
+            .substringBeforeLast('\n')
+            .split(Regex(",\\s*"))
+            .map { it.toInt() }
     }
 
-    override fun executePart2(): String {
-        return "Day 6 (part 2) not implemented yet"
+    private fun populate(iterations: Int): Long {
+        val input = getInput().groupBy { it }
+            .mapValues { (_, v) -> v.size.toLong() }
+
+        val fish = LongArray(9) { input[it] ?: 0L }
+
+        repeat(iterations) {
+            val newFish = fish[0]
+            for (age in 1..8) {
+                fish[age - 1] = fish[age]
+            }
+            fish[8] = newFish
+            fish[6] = fish[6] + newFish
+        }
+        return fish.sum()
+    }
+
+    override fun executePart1(): Long {
+        return populate(80)
+    }
+
+    override fun executePart2(): Long {
+        return populate(256)
     }
 }
